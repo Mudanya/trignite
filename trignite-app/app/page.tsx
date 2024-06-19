01e9ecac4db1519e09e8dae42d845032c684b5f3
Mudanya <nmudanya94@gmail.com>
2024-06-19 23:42:10 +0300

    Initial Commit

---
+++ trignite-app/app/page.tsx
@@ -0,0 +1,11 @@
+import GridBackground from "@/components/ui/GridBackground";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="dark w-screen h-screen flex justify-center items-center">
+      {/* Todo just placeholder */}
+     <GridBackground />
+    </main>
+  );
+}
